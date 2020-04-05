c92ae3c00240c76d76ebe6eb3816fb00de496b59
Guillaume <g.suppin@mailo.com>
2020-04-05 17:05:38 +0200

    suppression @csrf

--- resources/views/enfants/create.blade.php
+++ resources/views/enfants/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h3 class="my-3">Ajouter un enfant</h3>
     <form class="my-3" action="{{ route('enfants.store') }}" method="post">
-        @csrf
+
         @include('enfants.form')
         <button class="btn btn-secondary" type="submit">Ajouter</button>
     </form>

17993436ad2fe203cb58f609242b6b986f755895
Guillaume <g.suppin@mailo.com>
2020-04-16 17:33:45 +0200

    ajout modele CategorieTache.php modification migration

---
+++ app/CategorieTache.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CategorieTache extends Model
+{
+    use SoftDeletes;
+    protected $fillable = ['libelle'];
+
+}
--- resources/views/catergoriestaches/index.blade.php
+++ resources/views/catergoriestaches/index.blade.php
@@ -7,17 +7,15 @@
         <thead>
         <tr>
             <th scope="col">#</th>
-            <th scope="col">Prénom</th>
-            <th scope="col">Nom</th>
+            <th scope="col">Libellé</th>
             <th scope="col">Action</th>
         </tr>
         </thead>
         <tbody>
-        @foreach ($responsables as $responsable)
+        @foreach ($categoriestaches as $categorietache)
             <tr>
-                <th scope="row">{{ $responsable->id }}</th>
-                <td>{{ $responsable->prenom }}</td>
-                <td>{{ $responsable->nom }}</td>
+                <th scope="row">{{ $categorietache->id }}</th>
+                <td>{{ $categorietache->libelle }}</td>
                 <td>
                     <a class="btn btn-secondary mb-1" href="{{ route('categoriestaches.edit',$responsable->id) }}"><i class="ri-pencil-line"></i>    </a>
                     <a class="btn btn-danger mb-1" href="{{ route('categoriestaches.confirmation',$responsable->id) }}"><i class="ri-delete-bin-7-line"></i>  </a>

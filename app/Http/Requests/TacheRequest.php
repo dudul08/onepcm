11025719f27ebc40afcdc5bea0fe0046a92df27d
Guillaume <g.suppin@mailo.com>
2020-04-18 15:27:27 +0200

    correction divers

--- app/Http/Requests/TacheRequest.php
+++ app/Http/Requests/TacheRequest.php
@@ -27,6 +27,6 @@ class TacheRequest extends FormRequest
     }
     public function messages()
     {
-        return ['libelle.required'=>'Le libellé est obligatoire !','nombre_points.required'=>'Le nombre de point est obligatoire','nombre_points_bonus.required'=>'Le nombre de point bonus est obligatoire','nombre_points.integer'=>'Cette information est incorrecte !','nombre_points_bonus.integer'=>'Cette information est incorrecte'];
+        return ['libelle.required'=>'Le libellé est obligatoire !','nombre_points.required'=>'Le nombre de point est obligatoire !','nombre_points_bonus.required'=>'Le nombre de point bonus est obligatoire !','nombre_points.integer'=>'Cette information est incorrecte !','nombre_points_bonus.integer'=>'Cette information est incorrecte !'];
     }
 }

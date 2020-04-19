4b5401b74857556fc8b85d73bd2d91cf241aa2ff
Guillaume <g.suppin@mailo.com>
2020-04-19 19:23:42 +0200

    ajout du middelware auth sur le groupe des routes

--- routes/web.php
+++ routes/web.php
@@ -12,26 +12,28 @@
 |
 */
 Route::view('/', 'accueil.index')->name('accueil');
-Route::resource('enfants', 'EnfantController')->except([
-    'show'
-]);
-Route::resource('responsables', 'ResponsableController')->except([
-    'show'
-]);
-Route::resource('taches', 'TacheController')->except([
-    'show'
-]);
-Route::resource('bonspoints', 'BonPointController')->except([
-    'show']);
-Route::resource('categoriestaches', 'CategorieTacheController')->except([
-    'show'
-]);
 
-Route::get('enfants/{enfant}/confirmation', 'EnfantController@confirmation')->name('enfants.confirmation');
-Route::get('responsables/{responsable}/confirmation', 'ResponsableController@confirmation')->name('responsables.confirmation');
-Route::get('categoriestaches/{categorietache}/confirmation', 'CategorieTacheController@confirmation')->name('categoriestaches.confirmation');
-Route::get('taches/{tache}/confirmation', 'TacheController@confirmation')->name('taches.confirmation');
+Route::group(['middleware' => 'auth'],function (){
+    Route::resource('enfants', 'EnfantController')->except([
+        'show'
+    ]);
+    Route::resource('responsables', 'ResponsableController')->except([
+        'show'
+    ]);
+    Route::resource('taches', 'TacheController')->except([
+        'show'
+    ]);
+    Route::resource('bonspoints', 'BonPointController')->except([
+        'show']);
+    Route::resource('categoriestaches', 'CategorieTacheController')->except([
+        'show'
+    ]);
 
+    Route::get('enfants/{enfant}/confirmation', 'EnfantController@confirmation')->name('enfants.confirmation');
+    Route::get('responsables/{responsable}/confirmation', 'ResponsableController@confirmation')->name('responsables.confirmation');
+    Route::get('categoriestaches/{categorietache}/confirmation', 'CategorieTacheController@confirmation')->name('categoriestaches.confirmation');
+    Route::get('taches/{tache}/confirmation', 'TacheController@confirmation')->name('taches.confirmation');
+});
 
 Auth::routes(['register' => false]);
 

3ec696cb17de1e3ff7d1aed04001f7de24f54e38
Guillaume <g.suppin@mailo.com>
2020-04-19 17:57:48 +0200

    correction texte dans accueil.index

--- resources/views/accueil/index.blade.php
+++ resources/views/accueil/index.blade.php
@@ -7,7 +7,7 @@
         <div class="col-12 mx-auto text-center ">
             <h1-effet>
                 <span-effet class="font-effet align-middle">On n'est pas</span-effet>
-                <span-effet class="font-effet align-middle"> chez Mamie !</span-effet>
+                <span-effet class="font-effet align-middle"> chez mamie !</span-effet>
             </h1-effet>
         </div>
     </div>

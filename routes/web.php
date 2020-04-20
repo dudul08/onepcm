63db988d183734ca4caa55e88e0128682b18dd9e
Guillaume <g.suppin@mailo.com>
2020-04-20 23:27:26 +0200

    pour taches : creation de toutes les routes restfull manuellement afin de ne pas avoir d'erreur sur le paramètre

--- routes/web.php
+++ routes/web.php
@@ -17,30 +17,34 @@ Route::group(['middleware' => 'auth'],function (){
     Route::resource('enfants', 'EnfantController')->except([
         'show'
     ]);
+    Route::get('enfants/{enfant}/confirmation', 'EnfantController@confirmation')->name('enfants.confirmation');
     Route::resource('responsables', 'ResponsableController')->except([
         'show'
     ]);
-    Route::resource('taches', 'TacheController')->except([
-        'show','edit','update','index'
-    ]);
+    Route::get('responsables/{responsable}/confirmation', 'ResponsableController@confirmation')->name('responsables.confirmation');
+
+
     Route::get('taches', 'TacheController@index')->name('taches.index');
+    Route::post('taches', 'TacheController@store')->name('taches.store');
+    Route::get('taches/create', 'TacheController@create')->name('taches.create');
     Route::get('taches/{tache}/edit', 'TacheController@edit')->name('taches.edit');
     Route::patch('taches/{tache}', 'TacheController@update')->name('taches.update');
-
-
+    Route::delete('taches/{tache}','TacheController@destroy')->name('taches.destroy');
+    Route::get('taches/{tache}/confirmation', 'TacheController@confirmation')->name('taches.confirmation');
 
 
 
     Route::resource('bonspoints', 'BonPointController')->except([
         'show']);
+
     Route::resource('categoriestaches', 'CategorieTacheController')->except([
         'show'
     ]);
-
-    Route::get('enfants/{enfant}/confirmation', 'EnfantController@confirmation')->name('enfants.confirmation');
-    Route::get('responsables/{responsable}/confirmation', 'ResponsableController@confirmation')->name('responsables.confirmation');
     Route::get('categoriestaches/{categorietache}/confirmation', 'CategorieTacheController@confirmation')->name('categoriestaches.confirmation');
-    Route::get('taches/{tache}/confirmation', 'TacheController@confirmation')->name('taches.confirmation');
+
+
+
+
 });
 
 Auth::routes(['register' => false]);

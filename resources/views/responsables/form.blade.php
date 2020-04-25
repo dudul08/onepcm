8a5abe131b54eacfb9b2c4b6d2e822d8246450f0
Guillaume <g.suppin@mailo.com>
2020-04-25 14:42:18 +0200

    ajout de l'email et du password

--- resources/views/responsables/form.blade.php
+++ resources/views/responsables/form.blade.php
@@ -12,16 +12,39 @@
     </div>
 </div>
 <div class="form-group row">
-    <label class="col-sm-2 col-form-label" for="nome">Nom : </label>
+        <label class="col-sm-2 col-form-label" for="nome">Nom : </label>
+        <div class="col-sm-5">
+            <input class="form-control @error('name') is-invalid @enderror" type="text" name="name"
+                   value="{{ old('name') ?? $responsable->nom }}">
+            @error('name')
+            <div class="invalid-feedback">
+                {{ $errors->first('name') }}
+            </div>
+            @enderror
+        </div>
+</div>
+<div class="form-group row">
+    <label class="col-sm-2 col-form-label" for="nome">Email : </label>
     <div class="col-sm-5">
-        <input class="form-control @error('nom') is-invalid @enderror" type="text" name="nom"
-               value="{{ old('nom') ?? $responsable->nom }}">
-        @error('nom')
+        <input class="form-control @error('email') is-invalid @enderror" type="text" name="email"
+               value="{{ old('email') ?? $responsable->email }}">
+        @error('email')
         <div class="invalid-feedback">
-            {{ $errors->first('nom') }}
+            {{ $errors->first('email') }}
         </div>
         @enderror
     </div>
 </div>
-
+<div class="form-group row">
+    <label class="col-sm-2 col-form-label" for="nome">Mot de passe : </label>
+    <div class="col-sm-5">
+        <input class="form-control @error('password') is-invalid @enderror" type="text" name="password"
+               value="{{ old('password') ?? $responsable->password }}">
+        @error('password')
+        <div class="invalid-feedback">
+            {{ $errors->first('password') }}
+        </div>
+        @enderror
+    </div>
+</div>
 

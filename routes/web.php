bc236ee5afa7d323fb42cb07a11b64da126bf2cc
Guillaume <g.suppin@mailo.com>
2020-04-25 18:49:27 +0200

    route vers le home désactivé

--- routes/web.php
+++ routes/web.php
@@ -57,4 +57,4 @@ Route::group(['middleware' => 'auth'],function (){
 
 Auth::routes(['register' => false]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');

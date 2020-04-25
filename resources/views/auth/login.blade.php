3b7ac66ec474be6c40c778337e3e0f2b9cda3622
Guillaume <g.suppin@mailo.com>
2020-04-25 14:41:55 +0200

    le nom est remplacé par le prénom

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,9 +15,9 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">Nom</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="prenom" type="text" class="form-control @error('prenom') is-invalid @enderror" name="prenom" value="{{ old('prenom') }}" required autocomplete="prenom" autofocus>
 
-                                @error('name')
+                                @error('prenom')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>

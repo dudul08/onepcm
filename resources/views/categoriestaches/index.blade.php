cda1ed9e5e6fdfb25b69672d41902c5651b6c184
Guillaume <g.suppin@mailo.com>
2020-04-25 18:48:37 +0200

    ajout card

--- resources/views/categoriestaches/index.blade.php
+++ resources/views/categoriestaches/index.blade.php
@@ -1,28 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-    <a class="btn btn-primary my-3" href="{{ route('categoriestaches.create') }}"><i class="ri-group-line"></i> Ajouter une catégorie</a>
+    <div class="card">
+        <div class="card-header text-center">
+            <h2>Liste des catégories des tâches</h2>
+        </div>
+        <div class="card-body">
+            <a class="btn btn-primary mb-3" href="{{ route('categoriestaches.create') }}"><i class="ri-group-line"></i>
+                Ajouter une catégorie</a>
 
-    <table class="table table-hover table-bordered">
-        <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Libellé</th>
-            <th scope="col">Action</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach ($categoriesTaches as $categorieTache)
-            <tr>
-                <th scope="row">{{ $categorieTache->id }}</th>
-                <td>{{ $categorieTache->libelle }}</td>
-                <td>
-                    <a class="btn btn-secondary mb-1" href="{{ route('categoriestaches.edit',$categorieTache->id) }}"><i class="ri-pencil-line"></i>    </a>
-                    <a class="btn btn-danger mb-1" href="{{ route('categoriestaches.confirmation',$categorieTache->id) }}"><i class="ri-delete-bin-7-line"></i>  </a>
-                </td>
+            <table class="table table-hover table-bordered">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Libellé</th>
+                    <th scope="col">Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach ($categoriesTaches as $categorieTache)
+                    <tr>
+                        <th scope="row">{{ $categorieTache->id }}</th>
+                        <td>{{ $categorieTache->libelle }}</td>
+                        <td>
+                            <a class="btn btn-secondary mb-1"
+                               href="{{ route('categoriestaches.edit',$categorieTache->id) }}"><i
+                                    class="ri-pencil-line"></i> </a>
+                            <a class="btn btn-danger mb-1"
+                               href="{{ route('categoriestaches.confirmation',$categorieTache->id) }}"><i
+                                    class="ri-delete-bin-7-line"></i> </a>
+                        </td>
 
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection

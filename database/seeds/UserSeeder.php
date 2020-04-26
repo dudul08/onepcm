d940be57be448e17bae49ba1aff595560b4a66c0
Guillaume <g.suppin@mailo.com>
2020-04-26 17:10:16 +0200

    ajout admin

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,35 +13,57 @@ class UserSeeder extends Seeder
     {
         //factory('App\User')->create();
         $parent = DB::table('users')->insert([
-            'name'=>'admin',
-            'email'=>'admin@admin.fr',
-            'password'=>\Illuminate\Support\Facades\Hash::make('admin'),
-            'is_admin'=>true,
-            'prenom'=>'admin',
-            'date_naissance'=>'1976-10-16',
-            'genre'=>'1',
-            'pseudo'=>'admin'
+            'name' => 'admin',
+            'email' => 'admin@admin.fr',
+            'password' => \Illuminate\Support\Facades\Hash::make('admin'),
+            'is_admin' => true,
+            'prenom' => 'admin',
+            'date_naissance' => '1976-10-16',
+            'genre' => '1',
+            'pseudo' => 'admin'
+
+        ]);
+        $parent = DB::table('users')->insert([
+            'name' => 'SUPPIN',
+            'email' => 'admin@admin.fr',
+            'password' => \Illuminate\Support\Facades\Hash::make('admin'),
+            'is_admin' => true,
+            'prenom' => 'Guillaume',
+            'date_naissance' => '1976-10-16',
+            'genre' => '1',
+            'pseudo' => 'papounet'
+
+        ]);
+        $parent = DB::table('users')->insert([
+            'name' => 'SUPPIN',
+            'email' => 'admin@admin.fr',
+            'password' => \Illuminate\Support\Facades\Hash::make('admin'),
+            'is_admin' => true,
+            'prenom' => 'Laure',
+            'date_naissance' => '1976-05-16',
+            'genre' => '0',
+            'pseudo' => 'mamounette'
 
         ]);
         $enfant = DB::table('users')->insert([
-            'name'=>'SUPPIN',
-            'email'=>'enfant@enfant.fr',
-            'password'=>\Illuminate\Support\Facades\Hash::make('enfant'),
-            'is_admin'=>false,
-            'prenom'=>'Elise',
-            'date_naissance'=>'2008-07-30',
+            'name' => 'SUPPIN',
+            'email' => 'enfant@enfant.fr',
+            'password' => \Illuminate\Support\Facades\Hash::make('enfant'),
+            'is_admin' => false,
+            'prenom' => 'Elise',
+            'date_naissance' => '2008-07-30',
             'genre' => '0',
-            'pseudo'=>'lili'
+            'pseudo' => 'lili'
         ]);
         $enfant = DB::table('users')->insert([
-            'name'=>'SUPPIN',
-            'email'=>'enfant@enfant.fr',
-            'password'=>\Illuminate\Support\Facades\Hash::make('enfant'),
-            'is_admin'=>false,
-            'prenom'=>'Baptiste',
-            'date_naissance'=>'2007-04-20',
+            'name' => 'SUPPIN',
+            'email' => 'enfant@enfant.fr',
+            'password' => \Illuminate\Support\Facades\Hash::make('enfant'),
+            'is_admin' => false,
+            'prenom' => 'Baptiste',
+            'date_naissance' => '2007-04-20',
             'genre' => '1',
-            'pseudo'=>'baptou'
+            'pseudo' => 'baptou'
         ]);
     }
 }

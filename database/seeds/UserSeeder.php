40257fbdd0f8bc5fc357eb4b8f3c139899cd09de
Guillaume <g.suppin@mailo.com>
2020-04-26 16:53:14 +0200

    ajout de pseudo dans le seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -19,17 +19,29 @@ class UserSeeder extends Seeder
             'is_admin'=>true,
             'prenom'=>'admin',
             'date_naissance'=>'1976-10-16',
-            'genre'=>'1'
+            'genre'=>'1',
+            'pseudo'=>'admin'
 
         ]);
         $enfant = DB::table('users')->insert([
-            'name'=>'enfant',
+            'name'=>'SUPPIN',
             'email'=>'enfant@enfant.fr',
             'password'=>\Illuminate\Support\Facades\Hash::make('enfant'),
             'is_admin'=>false,
-            'prenom'=>'enfant',
-            'date_naissance'=>'2007-08-20',
-            'genre' => '0'
+            'prenom'=>'Elise',
+            'date_naissance'=>'2008-07-30',
+            'genre' => '0',
+            'pseudo'=>'lili'
+        ]);
+        $enfant = DB::table('users')->insert([
+            'name'=>'SUPPIN',
+            'email'=>'enfant@enfant.fr',
+            'password'=>\Illuminate\Support\Facades\Hash::make('enfant'),
+            'is_admin'=>false,
+            'prenom'=>'Baptiste',
+            'date_naissance'=>'2007-04-20',
+            'genre' => '1',
+            'pseudo'=>'baptou'
         ]);
     }
 }

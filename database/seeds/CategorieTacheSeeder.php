0f836594feb807244e0c926dc59b82cc2b5820fa
Guillaume <g.suppin@mailo.com>
2020-05-25 00:52:50 +0200

    modification seeder

--- database/seeds/CategorieTacheSeeder.php
+++ database/seeds/CategorieTacheSeeder.php
@@ -13,12 +13,15 @@ class CategorieTacheSeeder extends Seeder
     {
        // factory('App\CategorieTache', 15)->create();
         $categorie_tache = DB::table('categorie_taches')->insert([
+            'id'=>1,
             'libelle' => 'Entretien extérieur'
         ]);
         $categorie_tache = DB::table('categorie_taches')->insert([
+            'id'=>2,
             'libelle' => 'Ménage'
         ]);
         $categorie_tache = DB::table('categorie_taches')->insert([
+            'id'=>3,
             'libelle' => 'Rangement'
         ]);
     }

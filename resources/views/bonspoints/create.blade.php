bcf8470e1503d1951f0bcd5cdf5b70c96da40052
Guillaume <g.suppin@mailo.com>
2020-05-28 00:02:04 +0200

    modification padding body

--- resources/views/bonspoints/create.blade.php
+++ resources/views/bonspoints/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="card">
-        <div class="card-header text-center">
+        <div class="card-header text-center ">
             <h3 class="my-3">Ajouter des bons points </h3>
         </div>
         <div class="card-body">
